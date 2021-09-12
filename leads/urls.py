8190ae05528a522c3c9260e48a7662d6be6b6e02
Nazmul Hossain <annazmul71@gmail.com>
2021-09-12 20:58:24 +0600

    lead_create lead_detail and lead_list view added

---
+++ leads/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from leads.views import lead_list, lead_detail, lead_create
+
+app_name = "leads"
+
+urlpatterns = [
+    path('', lead_list),
+    path('<int:pk>/', lead_detail),
+    path('create/', lead_create),
+]
--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from leads.models import Lead
 
 
-def home_page(request):
-    # return HttpResponse("Hello Universe")
-    return render(request, "leads/home_page.html")
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+    return render(request, "leads/lead_list.html", context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead": lead
+    }
+    return render(request, "leads/lead_detail.html", context)
+
+
+def lead_create(request):
+    return render(request, "leads/lead_create.html")
