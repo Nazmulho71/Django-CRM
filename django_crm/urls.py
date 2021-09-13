485d04300416bb1ac30e04c40d2412920143b491
Nazmul Hossain <annazmul71@gmail.com>
2021-09-13 19:22:02 +0600

    new design added for the pages

--- django_crm/urls.py
+++ django_crm/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from leads.views import landing_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_page, name="landin-_page"),
     path('leads/', include('leads.urls', namespace="leads"))
 ]
