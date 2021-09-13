87ce096fa3a495ab6d4b96cac5640d1035e882c6
Nazmul Hossain <annazmul71@gmail.com>
2021-09-13 11:31:22 +0600

    django form added

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from leads.models import Lead
+from leads.models import Lead, Agent
+from .forms import LeadForm
 
 
 def lead_list(request):
@@ -20,4 +21,23 @@ def lead_detail(request, pk):
 
 
 def lead_create(request):
-    return render(request, "leads/lead_create.html")
+    form = LeadForm()
+    if request.method == "POST":
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            age = form.cleaned_data["age"]
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                age=age,
+                agent=agent
+            )
+            return redirect('/leads')
+
+    context = {
+        "form": form
+    }
+    return render(request, "leads/lead_create.html", context)
