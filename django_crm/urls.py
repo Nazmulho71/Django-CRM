ccef54839680af9a99126227ac87e9d93b062564
Nazmul Hossain <annazmul71@gmail.com>
2021-09-14 15:28:37 +0600

    winerror 10013

--- django_crm/urls.py
+++ django_crm/urls.py
@@ -1,9 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from leads.views import landing_page, LandingPageView
+from leads import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', LandingPageView.as_view(), name="landin-page"),
+    path('', views.LandingPageView.as_view(), name="landin-page"),
     path('leads/', include('leads.urls', namespace="leads"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
